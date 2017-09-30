458ce623ea2b02c8da392388a62c88a8d01e060a
Rita <marharyta.dekret@gmail.com>
2017-09-30 14:26:21 +0300

    add images

--- ntag/urls.py
+++ ntag/urls.py
@@ -1,11 +1,14 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
 
-urlpatterns += staticfiles_urlpatterns()
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
 urlpatters = [
 	url(r'^$', views.TagAdd),
 	url(r'^$', views.TagDisplay),
 	url(r'^(?<tag_id>\d+)/$', views.TagDisplayCurrent)
 ]
+
+
+urlpatterns += patterns('', url(r'^(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT,}),)
